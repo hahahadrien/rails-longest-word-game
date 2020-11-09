ecac5c47aa4c16a2f4dec847cb602d72e9752de5
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-09 14:53:50 +0100

    longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+
+  def new
+    # The new action will be used to display a new random grid and a form.
+  end
+
+  def score
+    # The form will be submitted (with POST) to the score action.
+  end
+
+end
